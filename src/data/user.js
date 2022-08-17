c7f3a167be1b6e9abd3a8ea15ecb276bd344aa94
syretta99 <111412774+syretta99@users.noreply.github.com>
2022-08-17 05:39:40 +0000

    Add files via upload

---
+++ src/data/user.js
@@ -0,0 +1,12 @@
+import adonysImage from '../assets/me.png';
+
+const user = {
+  name: 'Hajijin Amri',
+  title: 'Junior Edit Web 😂',
+  img: {
+    src: adonysImage,
+    alt: 'Adonys Santos photo',
+  },
+};
+
+export default user;
